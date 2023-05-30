5870ceb169b2485fccd1f0471789ce1f03c4d325
Try Iksan <tryiksan026@student.unsrat.ac.id>
2023-05-30 22:04:23 +0800

    fix bug

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ const server = http.createServer((req, res) => {
     res.statusCode = 400;
     res.setHeader('Content-Type', 'text/plain');
     res.end('error: masukkan nilai radius parameter.\n');
+    res.end('Nama: Try Setiawan Iksan\nNIM:210211060199\nTIK1032-D');
   } else {
     const area = Math.PI * radius ** 2;
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end(`luas lingkaran dengan radius ${radius} yaitu ${area}.\n`);
+    res.end('Nama: Try Setiawan Iksan\nNIM:210211060199\nTIK1032-D');
   }
 });
-
-console.log('Nama: Try Setiawan Iksan\nNIM:210211060199\nTIK1032-D');
 
 server.listen(3000, () => {
   console.log('Server running at http://localhost:3000/');
