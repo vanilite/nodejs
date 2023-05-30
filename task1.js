baad5dd7dc35384db63707b4be019ae7fd84074c
Try Iksan <tryiksan026@student.unsrat.ac.id>
2023-05-30 11:31:09 +0800

    with radius

---
+++ task1.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+  const queryObject = url.parse(req.url, true).query;
+  const radius = queryObject.radius;
+
+  if (radius === undefined) {
+    res.statusCode = 400;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Error: Please provide a radius parameter in your query string.\n');
+  } else {
+    const area = Math.PI * radius ** 2;
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`The area of a circle with radius ${radius} is ${area}.\n`);
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/?radius=5');
+});
